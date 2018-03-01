326d6887f8d8e5b42593a0d704eb1294d5c98390
Brendan Richards <brendan_richards_msdn@hotmail.com>
2018-03-01 11:19:50 +1100

    added ngmodel

--- firebootcamp-crm/src/app/app.component.ts
+++ firebootcamp-crm/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fbc';
+
+valueChanged(event) {
+  console.log('value changed');
+  this.title = event.target.value;
+}
+
 }
