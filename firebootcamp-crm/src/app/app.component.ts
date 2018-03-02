968fab60473edcb9caba85e8e9d4caef0281ce6a
Brendan Richards <brendan_richards_msdn@hotmail.com>
2018-03-02 12:56:05 +1100

    made state service

--- firebootcamp-crm/src/app/app.component.ts
+++ firebootcamp-crm/src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CompanyService } from './company/company.service';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators/map';
+import { filter } from 'rxjs/operators/filter';
 
 @Component({
   selector: 'fbc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'fbc';
+
+  companiesCount$: Observable<number>;
+
+  constructor(
+    private companyService: CompanyService
+  ){}
+
+  ngOnInit(): void {
+    this.companiesCount$ = this.companyService.getCompanies()
+      .pipe(
+       filter(c => c.length > 0),
+       map(c => c.length)
+      );
+  }
 
 }
